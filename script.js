7dc8ba54c9c2ce44e9bf5840259777c4de591e35
swarnaprony <swarnaprony81@gmail.com>
2023-09-08 13:17:53 +0200

    Hi

--- script.js
+++ script.js
@@ -12,32 +12,44 @@ function revealPregnancy() {
         // Add more names in lowercase and messages as needed
     };
 
+    const headingElement = document.querySelector("h1"); // Get the heading element
+
     if (familyName) {
+        // Change the heading to "Surprise!"
+        headingElement.textContent = "Surprise!";
+
         greetingElement.textContent = ""; // Clear any previous text
         hiddenImageElement.style.display = "none"; // Hide the image
 
         let message = personalizedMessages[familyName];
-        if (!message) {
-            message = `Hello, ${familyName}! We have exciting news to share.`;
-        }
-
-        let i = 0;
-        const revealInterval = setInterval(function() {
+        if (message) {
+            let i = 0;
+        const revealInterval = setInterval(function () {
             greetingElement.textContent += message[i];
             i++;
             if (i > message.length - 1) {
                 clearInterval(revealInterval); // Stop the interval when text is fully revealed
 
                 // Delay the image reveal by 1 second (1000 milliseconds)
-                setTimeout(function() {
+                setTimeout(function () {
                     hiddenImageElement.style.display = "block"; // Show the image
                 }, 1000); // Adjust the delay time as needed
+
+                // Change the heading to "Surprise!"
+                headingElement.textContent = "Surprise!";
             }
         }, revealSpeed);
+        } else {
+            greetingElement.textContent = `Enter your name again`;
+        }
+
+
     } else {
         greetingElement.textContent = "Please enter your name before clicking 'Submit'.";
     }
- }
+}
+
+
 
  // Function to handle the "Enter" key press
 function handleEnterKeyPress(event) {
